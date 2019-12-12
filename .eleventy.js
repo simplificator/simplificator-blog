9c0b22561851bf26ef31f134bdb172c52a401c18
Dimiter Petrov <crackofdusk@gmail.com>
2019-12-12 08:43:46 +0100

    Add critical CSS inline

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
+const CleanCSS = require("clean-css")
+
 module.exports = (config) => {
   config.addPassthroughCopy("content/images")
+
+  config.addShortcode('embeddedStyle', (path) => {
+    let completePath = `content/_includes/${path}`
+    return new CleanCSS({}).minify([completePath]).styles
+  })
 
   return {
     dir: {
